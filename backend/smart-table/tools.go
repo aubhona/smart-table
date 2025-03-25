287ccfece745742ee48261a7fc4120240e884506
Aubhon <90551096+aubhona@users.noreply.github.com>
2025-03-25 17:35:54 +0300

    cc: make clean

--- backend/smart-table/tools.go
+++ backend/smart-table/tools.go
@@ -5,4 +5,7 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+	_ "github.com/sqlc-dev/sqlc/cmd/sqlc"
+	_ "github.com/vektra/mockery/v2"
 )
